d84b291e64f924417f07869b8c2f2d22bf39c211
Kemar Cockburn <kemar_cockburn@comcast.com>
2016-10-18 16:32:56 -0400

    adding download test to repo

--- public/examples/download/downloadTest.js
+++ public/examples/download/downloadTest.js
@@ -4,8 +4,9 @@
 
     //test button node will be made available through this variable
     var testButton;
-    var versions = [];
+    var currentTest;
     var auditTrail;
+    var testRunner = [];
     //event binding method for buttons
     function addEvent(el, ev, fn) {
         void (el.addEventListener && el.addEventListener(ev, fn, false));
@@ -14,45 +15,73 @@
     }
 
     //callback for xmlHttp complete event
-    function downloadHttpOnComplete(result) {
-        testButton.disabled = false;
-        auditTrail.push({ event: 'downloadHttpOnComplete', result: result });
-        //an array of results are returned
-        //we return the lowest calculated value
-        var arr = result.sort(function (a, b) {
-            return +a.time - +b.time;
-        });
-        //display to end user
-        console.log(arr);
-        //document.querySelector('.download').value = arr[0].time + ' ms';
+    function downloadHttpOnComplete(version, result) {
+        auditTrail.push({ event: version + ': downloadHttpOnComplete', result: result });
+        document.querySelector('.download-' + version).value = result + ' Mbps';
         displayAuditTrail();
+        var next = testRunner.shift();
+        if (next) {
+            next.start();
+            return;
+        }
+        //restore ability to choose tests again
+        testButton.disabled = false;
+        var testTypes = document.querySelectorAll('input[name = "testType"]');
+        for (var i = 0; i < testTypes.length; i++) {
+            testTypes[i].disabled = false;
+        }
     }
 
     //callback for xmlHttp progress event
-    function downloadHttpOnProgress(result) {
+    function downloadHttpOnProgress(version, result) {
+        auditTrail.push({ event: version + ': downloadHttpOnProgress', result: result });
+        displayAuditTrail();
+        //restore ability to run tests again
         testButton.disabled = false;
-        auditTrail.push({ event: 'downloadHttpOnProgress', result: result });
     }
 
     //callback for xmlHttp abort event
-    function downloadHttpOnAbort(result) {
-        testButton.disabled = false;
-        auditTrail.push({ event: 'downloadHttpOnAbort', result: result });
+    function downloadHttpOnAbort(version, result) {
+        auditTrail.push({ event: version + ': downloadHttpOnAbort', result: result });
         displayAuditTrail();
+
+        //restore ability to run tests again
+        testButton.disabled = false;
     }
 
     //callback for xmlHttp timeout event
-    function downloadHttpOnTimeout(result) {
-        testButton.disabled = false;
-        auditTrail.push({ event: 'downloadHttpOnTimeout', result: result });
+    function downloadHttpOnTimeout(version, result) {
+        auditTrail.push({ event: version + ': downloadHttpOnTimeout', result: result });
         displayAuditTrail();
+        var next = testRunner.shift();
+
+        //restore ability to return tests again
+        testButton.disabled = false;
+
+        //restore ability to choose test type again
+        var testTypes = document.querySelectorAll('input[name = "testType"]');
+        for (var i = 0; i < testTypes.length; i++) {
+            testTypes[i].disabled = false;
+        }
     }
 
     //callback for xmlHttp error event
-    function downloadHttpOnError(result) {
-        testButton.disabled = false;
-        auditTrail.push({ event: 'downloadHttpOnError', result: result });
+    function downloadHttpOnError(version, result) {
+        auditTrail.push({ event: version + ': downloadHttpOnError', result: result });
         displayAuditTrail();
+        var next = testRunner.shift();
+        if (next) {
+            next.start();
+            return;
+        }
+        //restore ability to return tests again
+        testButton.disabled = false;
+
+        //restore ability to choose test type again
+        var testTypes = document.querySelectorAll('input[name = "testType"]');
+        for (var i = 0; i < testTypes.length; i++) {
+            testTypes[i].disabled = false;
+        }
     }
 
     //displays event trail from start to completion and they api results at those different points
@@ -78,51 +107,87 @@
     function initDownloadTest() {
         //update testButton variable with testButton dom node reference
         testButton = document.querySelector('.action-start');
-        var auditButton = document.querySelector('.action-audit-trail');
 
         //register click event for http download tests
         var testTypes = document.querySelectorAll('input[name = "testType"]');
-        document.querySelector('.events').innerHTML = 'Click "Run Test" to begin';
-
-        for (var i = 0; i < testTypes.length; i++) {
-            addEvent(testTypes[i], 'click', function () {
+        document.querySelector('.events').innerHTML = 'No Event Trail. <p>Click "Run Test" to begin</p>';
+        var callback = function (version, func) {
+            return function (event) {
+                func.call(this, event, version);
+            };
+        };
+        for (var i = 0, fields, checked; i < testTypes.length; i++) {
+            addEvent(testTypes[i], 'click', callback(testTypes[i].value, function (e, version) {
+                var events = document.querySelector('.events');
+                var el = e.target || e.srcElement;
+                var checked = el.checked;
+                var relatedEl = document.querySelectorAll('.' + version);
+                var resultsEl = document.querySelector('.download-' + version);
+                var display = el.style.display;
+                var value = el.value;
                 //reset audit trail
                 //reset audit trail list
-                document.querySelector('.events').innerHTML = 'Click "Run Test" to begin';
+                events.innerHTML = 'No Event Trail. <p>Click "Run Test" to begin</p>';
                 //reset lowest download value field
-                document.querySelector('.download-IPv4').value = '';
-                document.querySelector('.download-IPv6').value = '';
-            });
+                resultsEl.style.display = (checked) ? 'block' : 'none';
+
+                //clear both result types
+                var resultsEl = document.querySelector('.download-IPv4').value = '';
+                var resultsEl = document.querySelector('.download-IPv6').value = '';
+
+                //toggle all related elements
+                for (var i = 0; i < relatedEl.length; i++) {
+                    relatedEl[i].style.display = (checked) ? 'block' : 'none';
+                }
+            }));
+
+            //filelds related to test type (i.e. ipv4, ipv6).
+            fields = document.querySelectorAll('.' + testTypes[i].value);
+            for (var k = 0, checked; k < fields.length; k++) {
+                checked = testTypes[i].checked;
+                fields[k].style.display = (checked) ? 'block' : 'none';
+            }
         }
 
         //add click event on "run test" button
         addEvent(testButton, 'click', function (e) {
             //prevent default click action in browser;
+            var baseUrl = '';
             e.preventDefault();
-            
+
             testButton.disabled = true;
             //reset audit trail
             auditTrail = [];
             //reset audit trail list
-            document.querySelector('.events').innerHTML = 'Click "Run Test" to begin';
+            document.querySelector('.events').innerHTML = '';
             //get test type value
-            var testType = document.querySelector('input[name = "testType"]:checked').value;
+            var testTypes = document.querySelectorAll('input[name = "testType"]');
             //create an instance of downloadHttpTest
-            
-            //set IPv6 version here
-            var baseUrl = testType === 'IPv6'? '': '';
-            
-            var downloadHttpConcurrentTestSuite = new window.downloadHttpConcurrent(baseUrl + '/download?bufferSize=100000000', 'GET', 4, 15000, 10000, downloadHttpOnComplete, downloadHttpOnProgress,
-                downloadHttpOnAbort, downloadHttpOnTimeout, downloadHttpOnError);
 
-            downloadHttpConcurrentTestSuite.start();
-        });
-        
-        //add click event on "display audit trail" button
-        addEvent(auditButton, 'click', function (e) {
-            //prevent default click action in browser;
-            e.preventDefault();
-            displayAuditTrail();
+            //set IPv6 version here
+            var callback = function (version, func) {
+                return function (results) {
+                    func.call(this, version, results);
+                };
+            };
+
+            //disable the checkbox while test is running
+            //for all checked test types run the download test
+            for (var i = 0, testType, checked; i < testTypes.length; i++) {
+                checked = testTypes[i].checked;
+                testTypes[i].disabled = true;
+                if (checked) {
+                    testType = testTypes[i].value;
+                    baseUrl = (testType === 'IPv6') ? '' : '';
+                    testRunner.push(new window.downloadHttpConcurrent(baseUrl + '/download?bufferSize=100000000', 'GET', 4, 15000, 10000,
+                        callback(testType, downloadHttpOnComplete), callback(testType, downloadHttpOnProgress), callback(testType, downloadHttpOnAbort),
+                        callback(testType, downloadHttpOnTimeout), callback(testType, downloadHttpOnError)));
+                }
+            }
+            var next = testRunner.shift();
+            if (next) {
+                next.start();
+            }
         });
     }
 
