57a97ad248f417cb3875bba6ed64f56ecd38fa4e
Richard Helferty <richard_helferty@comcast.com>
2016-10-21 11:46:20 -0400

    Add Karma Test Suite and xmlhttprequest unit test

---
+++ karma.conf.js
@@ -0,0 +1,72 @@
+// Karma configuration
+// Generated on Fri Oct 21 2016 11:28:29 GMT-0400 (EDT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine-ajax','jasmine', 'requirejs'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'public/lib/*.js',
+      'public/test/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      'base/public/examples/*.*',
+      'base/public/index.html'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
---
+++ public/test/xmlhttprequest.test.js
@@ -0,0 +1,143 @@
+'use strict';
+
+
+
+describe('xmlhttprequest test functions and onComplete:', function() {
+
+    // test setup
+    beforeEach(function () {
+        jasmine.Ajax.install();
+    });
+
+    // test teardown
+    afterEach(function () {
+        jasmine.Ajax.uninstall();
+    });
+
+    it('should expose the xmlhttprequest start function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request.start).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _initiateRequest function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._initiateRequest).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleLoadstart function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleLoadstart).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleError function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleError).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleTimeout function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleTimeout).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleAbort function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleAbort).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest close function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request.close).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleOnReadyStateChange function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleOnReadyStateChange).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleLoad function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleLoad).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleOnProgressDownload function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleOnProgressDownload).toBe('function');
+    });
+
+    it('should expose the xmlhttprequest _handleOnProgressUpload function', function() {
+        var request = new window.xmlHttpRequest();
+        expect(typeof request._handleOnProgressUpload).toBe('function');
+    });
+
+    it('should return result in onComplete', function(done){
+            window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+            var id = 1;
+            function requestOnComplete(result){
+                expect(typeof result.time).toBe('number');
+                expect(result.id).toEqual(parseInt(id));
+            }
+            function requestOnProgress(result){
+            }
+            function requestOnAbort(result){
+            }
+            function requestOnTimeout(result){
+            }
+            function requestOnError(result){
+            }
+
+            var request = new window.xmlHttpRequest('GET', '/latencyTestPoint', 3000, requestOnComplete,
+                requestOnAbort, requestOnTimeout, requestOnError);
+            request.start(0,id);
+            // mock successful response
+            setTimeout(function() {
+                jasmine.Ajax.requests.mostRecent().respondWith({
+                    status: 200
+                });
+                done();
+            }, 0);
+
+        });
+});
+
+describe('SpeedTest Core JS Library: TestRequest (mocked time)', function() {
+
+    // test setup
+    beforeEach(function() {
+        jasmine.clock().install();
+        jasmine.Ajax.install();
+    });
+
+    // test teardown
+    afterEach(function() {
+        jasmine.Ajax.uninstall();
+        jasmine.clock().uninstall();
+    });
+
+    it('should fire timeout callback on request timeout', function() {
+        var testUrl = '/testendpoint';
+        jasmine.Ajax.stubRequest(testUrl).andTimeout();
+        var id = 1;
+        function requestOnComplete(result){
+        }
+        function requestOnProgress(result){
+        }
+        function requestOnAbort(result){
+        }
+        function requestOnTimeout(result){
+            //TODO return different object
+            expect(typeof result).toBe('object');
+        }
+        function requestOnError(result){
+        }
+
+        var request = new window.xmlHttpRequest('GET', testUrl, 0, requestOnComplete,
+            requestOnAbort, requestOnTimeout, requestOnError);
+        request.start(0,id);
+
+    });
+
+
+
+});
+
+
