22a20ff4f5c6f62857fd24b6d307757fd9a30ecc
Kemar Cockburn <kemar_cockburn@comcast.com>
2016-10-25 13:59:55 -0400

    fixing licensing duplication. adding use strict

--- public/examples/latency/latencyTest.js
+++ public/examples/latency/latencyTest.js
@@ -16,26 +16,9 @@
  * /
  */
 
-
-/*
- * *
- *  Copyright 2014 Comcast Cable Communications Management, LLC
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- * /
- */
-
 (function () {
+    'use strict';
+
     //setting the initialization method for latency test suite
     var oldOnload = window.onload;
     window.onload = function () {
@@ -290,7 +273,7 @@
                                 testRunner.push(new window.latencyHttpTest(baseUrl, 10, 30000, callback('http', testVersion, onComplete), callback('http', testVersion, onProgress),
                                     callback(testProtocol, testVersion, onAbort), callback(testVersion, onTimeout), callback('http', testVersion, onError)));
                                 //start latencyHttpTest
-                            }else if (testProtocol === 'webSocket' && (checked[testVersion] && checked[testProtocol])) {
+                            } else if (testProtocol === 'webSocket' && (checked[testVersion] && checked[testProtocol])) {
                                 //create an instance of latencyWebSocketTest
                                 baseUrl = [testPlan['webSocketUrl' + testVersion], '/latency'].join('');
                                 testRunner.push(new window.latencyWebSocketTest(baseUrl, 'GET', '0', '10', 3000, callback('webSocket', testVersion, onComplete),
