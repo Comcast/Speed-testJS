4e129cba9bc36e9fc93644d86164b0080ef26fae
Richard Helferty <richard_helferty@comcast.com>
2016-10-31 07:58:09 -0400

    format code

--- public/lib/uploadHttpConcurrent.js
+++ public/lib/uploadHttpConcurrent.js
@@ -16,7 +16,7 @@
  * /
  */
 
-(function() {
+(function () {
     'use strict';
     /**
      * Upload Bandwidth testing based on httpRequests
@@ -32,7 +32,7 @@
      * @param function callback function for test suite error event
      * @param integer uploadSize of the request
      */
-    function uploadHttpConcurrent(url, type, concurrentRuns, timeout, testLength,callbackComplete, callbackProgress,
+    function uploadHttpConcurrent(url, type, concurrentRuns, timeout, testLength, callbackComplete, callbackProgress,
                                   callbackError, uploadSize) {
         this.url = url;
         this.type = type;
@@ -67,8 +67,8 @@
      * @param object error object
      * @return error object
      */
-    uploadHttpConcurrent.prototype.onTestTimeout = function(error){
-        if(this._running){
+    uploadHttpConcurrent.prototype.onTestTimeout = function (error) {
+        if (this._running) {
             this.clientCallbackError(error);
             this._running = false;
         }
@@ -79,8 +79,8 @@
      * @param object error object
      * @return error object
      */
-    uploadHttpConcurrent.prototype.onTestAbort = function(error){
-        if(this._running){
+    uploadHttpConcurrent.prototype.onTestAbort = function (error) {
+        if (this._running) {
             if ((Date.now() - this._beginTime) > this.testLength) {
                 if (this.finalResults && this.finalResults.length) {
                     this.clientCallbackComplete(this.finalResults);
@@ -96,8 +96,8 @@
      * @param object error object
      * @return error object
      */
-    uploadHttpConcurrent.prototype.onTestError = function(error){
-        if(this._running){
+    uploadHttpConcurrent.prototype.onTestError = function (error) {
+        if (this._running) {
             this.clientCallbackError(error);
             this._running = false;
         }
@@ -105,15 +105,15 @@
     /**
      * onComplete method
      */
-    uploadHttpConcurrent.prototype.onTestComplete = function(result){
-        if(!this._running){
+    uploadHttpConcurrent.prototype.onTestComplete = function (result) {
+        if (!this._running) {
             return;
         }
         //populate array
         this._progressResults['arrayProgressResults' + result.id].push(result.bandwidth);
-        this._activeTests.pop(result.id,1);
-        if((Date.now() - this._beginTime)< this.testLength){
-            if(!this._activeTests.length && this._running){
+        this._activeTests.pop(result.id, 1);
+        if ((Date.now() - this._beginTime) < this.testLength) {
+            if (!this._activeTests.length && this._running) {
                 /*
                  var movingAverage = 0;
                  for (var z=1;z<=this.concurrentRuns;z++){
@@ -129,10 +129,10 @@
 
             }
         }
-        else{
+        else {
             this._running = false;
-            for(var i=0;i<this._activeTests.length;i++){
-                if (typeof(this._activeTests[i])!== 'undefined') {
+            for (var i = 0; i < this._activeTests.length; i++) {
+                if (typeof(this._activeTests[i]) !== 'undefined') {
                     this._activeTests[i].xhr._request.abort();
                 }
             }
@@ -148,7 +148,7 @@
         var totalMovingAverage = 0;
         for (var i = 0; i < this.concurrentRuns; i++) {
             // get array size and loop thru size of moving average series or array length
-            var id = this._testIndex -i;
+            var id = this._testIndex - i;
             var arrayData = 'arrayProgressResults' + id;
             var lastElem = Math.min(this._progressResults[arrayData].length, this.concurrentRuns);
             if (lastElem > 0) {
@@ -171,7 +171,7 @@
     /**
      * onProgress method
      */
-    uploadHttpConcurrent.prototype.onTestProgress = function(result){
+    uploadHttpConcurrent.prototype.onTestProgress = function (result) {
         //this._progressResults['arrayProgressResults'+result.id].push(result.bandwidth);
         //todo add moving average counter and formulate results and return to client
     };
@@ -179,38 +179,38 @@
     /**
      * Start the test
      */
-    uploadHttpConcurrent.prototype.start = function() {
-        if(!this._running){
+    uploadHttpConcurrent.prototype.start = function () {
+        if (!this._running) {
             return;
         }
         if (this.type === 'GET') {
             for (var g = 1; g <= this.concurrentRuns; g++) {
                 this._testIndex++;
 
-                this['arrayResults'+this._testIndex] = [];
-                this._progressResults['arrayProgressResults'+this._testIndex] = new Array();
-                var request = new window.xmlHttpRequest('POST',this.url,this.timeout, this.onTestComplete.bind(this), this.onTestProgress.bind(this),
-                    this.onTestAbort.bind(this),this.onTestTimeout.bind(this),this.onTestError.bind(this));
+                this['arrayResults' + this._testIndex] = [];
+                this._progressResults['arrayProgressResults' + this._testIndex] = new Array();
+                var request = new window.xmlHttpRequest('POST', this.url, this.timeout, this.onTestComplete.bind(this), this.onTestProgress.bind(this),
+                    this.onTestAbort.bind(this), this.onTestTimeout.bind(this), this.onTestError.bind(this));
                 this._activeTests.push({
                     xhr: request,
                     testRun: this._testIndex
                 });
-                request.start(0,this._testIndex);
+                request.start(0, this._testIndex);
 
             }
         }
         else {
             for (var p = 1; p <= this.concurrentRuns; p++) {
                 this._testIndex++;
-                this['arrayResults'+this._testIndex] = [];
-                this._progressResults['arrayProgressResults'+this._testIndex] = new Array();
-                var request = new window.xmlHttpRequest('POST',this.url,this.timeout, this.onTestComplete.bind(this), this.onTestProgress.bind(this),
-                    this.onTestAbort.bind(this),this.onTestTimeout.bind(this),this.onTestError.bind(this));
+                this['arrayResults' + this._testIndex] = [];
+                this._progressResults['arrayProgressResults' + this._testIndex] = new Array();
+                var request = new window.xmlHttpRequest('POST', this.url, this.timeout, this.onTestComplete.bind(this), this.onTestProgress.bind(this),
+                    this.onTestAbort.bind(this), this.onTestTimeout.bind(this), this.onTestError.bind(this));
                 this._activeTests.push({
                     xhr: request,
                     testRun: this._testIndex
                 });
-                request.start(this.uploadSize,this._testIndex);
+                request.start(this.uploadSize, this._testIndex);
             }
         }
     }
@@ -218,10 +218,10 @@
     /**
      * Cancel the test
      */
-    uploadHttpConcurrent.prototype.abortAll = function() {
+    uploadHttpConcurrent.prototype.abortAll = function () {
         this._running = false;
-        for(var i=0;i<this._activeTests.length;i++){
-            if (typeof(this._activeTests[i])!== 'undefined') {
+        for (var i = 0; i < this._activeTests.length; i++) {
+            if (typeof(this._activeTests[i]) !== 'undefined') {
                 this._activeTests[i].xhr._request.abort();
             }
         }
@@ -230,11 +230,11 @@
     /**
      * init test suite
      */
-    uploadHttpConcurrent.prototype.initiateTest = function(){
+    uploadHttpConcurrent.prototype.initiateTest = function () {
         this._testIndex = 0;
-        this._results.length=0;
-        this._finalResults.length=0;
-        this._activeTests.length=0;
+        this._results.length = 0;
+        this._finalResults.length = 0;
+        this._activeTests.length = 0;
         this._progressResults = {};
         this._progressCount = 0;
         this._running = true;
